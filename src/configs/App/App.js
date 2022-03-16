2455b88feee9db9c5393a76a4290d87c73b4e287
will363 <williamtokam78@gmail.com>
2022-03-16 21:06:17 +0100

    #[1]Premier rendu interface login (design)

--- src/configs/App/App.js
+++ src/configs/App/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Routes from "../../routes";
-//import LoginPage from '../../pages/CommonModule/Login';
+import LoginPage from '../../pages/CommonModule/Login';
 import axios from "axios";
 import {useDispatch} from "react-redux";
 import {logoutAction} from "../../redux/AuthReducer";
@@ -41,8 +41,8 @@ const App = () => {
 
   return (
       <>
-        <Routes/>
-        {/* <LoginPage /> */}
+        {/* <Routes/> */}
+        <LoginPage />
       </>
   );
 
--- src/pages/CommonModule/Login/LoginPage.jsx
+++ src/pages/CommonModule/Login/LoginPage.jsx
@@ -1,11 +1,40 @@
 import React from 'react';
+import Background from '../../../assets/images/background_vima.png'
+import '../Login/LoginPage.css'
 
 const LoginPage = () => {
 
     return (
-        <div>
-            <h1>Hello, World</h1>
-        </div>
+       <section className='section'> 
+               <div className='imgBx'>
+                    <img src={Background}/>
+               </div>
+               <div className='contentBx'>
+                   <div className='formBx'>
+                   <h1>Sign up</h1>
+                    <form>
+                        <div className='inputBx'>
+                            <label for='name'>Name</label><br/>
+                            <input type='text' placeholder='Yevheniia' />
+                        </div>
+                        <div className='inputBx'>
+                            <label for='email'>Email</label><br/>
+                            <input id='email' type='email' name='yevheniia@dribbble.com' placeholder='yevheniia@dribbble.com' />
+                        </div>
+                        <div className='inputBx'>
+                            <label for='password'>Password</label><br/>
+                            <input id='password' type='password' name='Yevheniia' placeholder='..........' />
+                        </div>
+                        <div className='inputBx'>
+                            <input type='submit' name='Sign up' value='Sign up' />
+                        </div>
+                        <div>
+                            <p>Already have an account? <a href='#'>Sign in</a></p>
+                        </div>
+                    </form>
+                   </div>
+               </div>
+       </section>
     );
 }
 
